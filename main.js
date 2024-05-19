983364ad441cf618c529143ce41b7df9badaaf21
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-20 00:12:42 +0200

    [fix]  input label, button to generate tickets and cancel button working

--- main.js
+++ main.js
@@ -1,58 +1,116 @@
 'use strict'
 
-const firstName = ;
-const lastName = ;
-const nameComplete = `${firstName} ${lastName}`;
-console.log(`Il nome completo è: ${nameComplete}`);
+console.log('sono connesso')
+
+const firstName = document.getElementById('firstName')
+const lastName =   document.getElementById('lastName')
+
+//oggetti del dom
+
+const buttonTicketGenerator = document.getElementById('ticketGenerator')
+const buttonDelete = document.getElementById('delete')
+const ticketContainer = document.getElementById('ticketContainer')
+const completeNameOutput = document.querySelector('.completeNameOutput')
+const carriageOutput = document.querySelector('.carriage')
+const cpCodeOutput = document.querySelector('.cp-code')
+const ticketCostResult= document.querySelector('.ticketCostResult')
+const offers = document.getElementById('offers')
 
 
-//il programma richiede un input all'utente tramite prompt:
-//-età, -numero km.--> queste 2 informazioni vanno usate come variabili.
 
-//età
-const age = ;
-const userAge= Number(age) ;
-console.log(`l'utente ha ${userAge} anni`);
-document.getElementById("user_age").innerHTML= " " + userAge + " anni" ;
+//funzione ButtonTicketGenerator
+buttonTicketGenerator.addEventListener('click', function(){
 
-//km - distance
-const km = ;
-const distance =Number(km) ;
-console.log(`distanza che deve percorrere: ${distance} km`);
-document.getElementById("distance").innerHTML= " " + distance +"km";
+    ticketContainer.classList.remove('d-none')
 
-//costo iniziale del biglietto
-const ticket = 0.21;
-console.log(`costo iniziale del biglietto ${ticket} €`);
+    //salvare il value delle variabili nome e cognome in una costante
+   const firstNameInput = firstName.value
+   const lastNameInput = lastName.value
 
-//in base  alle informazioni fornite calcola il prezzo, 0,21euro al km.
-const ticketCost =  ticket * distance ;
-console.log(`costo del biglietto: ${ticketCost.toFixed(2)}€`);
-document.getElementById("ticket_price").innerHTML= " "+ ticketCost.toFixed(2)+"€";
+    
+   //nome completo
+   const completeName = `${firstNameInput} ${lastNameInput}`
+   completeNameOutput.textContent = `${completeName}`
 
 
-// definissco variabile dello sconto a 0 e la modifico all interno delle condizioni
-let discount = 0;
+   //carrozza
+   const randomNumCarriage = Math.floor(Math.random() * 10) + 1
+   carriageOutput.textContent =  randomNumCarriage
 
-    //se l'utente è minore di 18 applico il 20% di sconto
-if (userAge < 18){
-    discount =  20;
+   //cp-code
+   const randomNumCpCode = Math.floor(Math.random() * 10000) + 1
+   cpCodeOutput.textContent = randomNumCpCode 
+
+   //age
+   const age = document.getElementById('age')
+   const userAge= Number(age.value)
+
+   //recupero i valori del km dall'input
+   const kmLabel = document.querySelector('.km-label').value
+   const distance = Number(kmLabel) 
+
+   //salvare costo iniziale e costo finale in una costante
+   const ticket = 0.21
+   const ticketCost =  ticket * distance 
+   
+   let discount = 0
+
+    //controllo 
+   if (isNaN(Number(kmLabel))) {
+    console.log('La distanza inserita non è valida.');
+    return;
+    }
+
+
+   if (userAge < 18){
+    discount =  20
     //creo una const con prezzo scontato, sottraendo  lo sconto al prezzo completo
-    const discountedPrice = ticketCost - (ticketCost * discount /100)
+    const discountedPrice = (ticketCost - (ticketCost * discount /100))
+    ticketCostResult.textContent = `${discountedPrice.toFixed(2)}€`
+    offers.textContent =`under 18`
     console.log(`è stato applicato il 20% di sconto: ${discountedPrice.toFixed(2)}€`);
 
     // se sopra i 65 va applicato uno sconto del 40%
-}else if (userAge >= 65){
-    discount = 40;
+    }else if (userAge >= 65){
+    discount = 40
     const discountedPrice = ticketCost - (ticketCost * discount /100)
-    console.log(`è stato applicato il 40% di sconto: ${discountedPrice.toFixed(2)}€`);
+    ticketCostResult.textContent = `${discountedPrice.toFixed(2)}€`
+    offers.textContent =`over 65`
+    console.log(`è stato applicato il 40% di sconto: ${discountedPrice.toFixed(2)}€`)
 
     // se è compreso tra 18 e 65 prezzo normale
-}else{
-    console.log(`è stato applicato il prezzo normale del biglietto: ${ticketCost.toFixed(2)}€`);
+    }else{
+    
+    ticketCostResult.textContent = `${ticketCost.toFixed(2)}€`
+    offers.textContent =`prezzo normale`
+    console.log(`è stato applicato il prezzo normale del biglietto: ${ticketCost.toFixed(2)}€`)
 
 }
 
-//l'output del prezzo è rielaborato con massimo 2 decimali.
+
+})
+
+
+buttonDelete.addEventListener('click',function(){
+
+    ticketContainer.classList.add('d-none')
+
+    firstName.value= ''
+    lastName.value= ''
+    age.value= ''
+    document.querySelector('.km-label').value = ''
+
+    completeNameOutput.textContent = '***'
+    carriageOutput.textContent = '****'
+    cpCodeOutput.textContent = '****'
+    ticketCostResult.textContent = '****'
+    offers.textContent = 'standard'
+})
+
+// console.log(`l'utente ha ${userAge} anni`)
+// console.log(`distanza che deve percorrere: ${distance} km`)
+// console.log(`costo iniziale del biglietto ${ticket} €`)
+// console.log(`costo del biglietto: ${ticketCost.toFixed(2)}€`)
+
 
 
---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const firstName = document.querySelector('#firstName');
+console.dir(firstName.value);
+
+firstName.value = " "
+
+const lastName = document.querySelector('#lastName');
+console.dir(lastName.value);
+
+lastName.value= " "
